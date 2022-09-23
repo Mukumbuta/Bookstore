ab332b42f67c2ef9eaf839bc2c5b1f938e7a5021
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-23 09:16:02 -0700

    add Books and Categories pages

---
+++ src/routes/Books.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DisplayBookList from '../components/DisplayBookList';
+
+const Books = () => (
+  <div>
+    <DisplayBookList />
+  </div>
+);
+
+export default Books;
---
+++ src/routes/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StatusButton from "../components/StatusButton";
+
+const Categories = () => {
+  return (
+    <div>
+      <StatusButton />
+    </div>
+  )
+}
+
+export default Categories
