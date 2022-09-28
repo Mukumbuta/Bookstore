2d6981a6bcd403de4cbf9d81233a92e0701d7f3f
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-28 16:18:16 -0700

    added book reducer; and action creators

---
+++ src/redux/books/book.js
@@ -0,0 +1,46 @@
+const BOOK_ADDED = 'BOOK_ADDED'
+const BOOK_REMOVED = 'BOOK_REMOVED'
+const initState = [];
+
+// Action Creator
+const addActionCreator = (title, author) => ({
+  type: BOOK_ADDED,
+  id,
+  title,
+  author,
+});
+
+const removeActionCreator = (title, author) => ({
+    type: BOOK_ADDED,
+    id,
+    title,
+    author,
+  });
+
+// Reducer
+const booksReducer = (state = initState, action) => {
+    switch (action.type) {
+        case BOOK_ADDED:
+            return [
+                ...state,
+               {
+                id: action.title,
+                title: action.title,
+                author: action.author
+               }
+
+            ];
+        case BOOK_REMOVED:
+            return state.map((book) => {
+                if (book.id !== action.id) {
+                    return book;
+                }
+            });
+        default:
+            return state;
+    }
+}
+
+export default booksReducer;
+export { addActionCreator, removeActionCreator };
+
