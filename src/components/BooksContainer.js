b61054d2de6582c1689d33af37ac0dedea2774ec
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-10-01 16:05:47 -0700

    used the useEffect hook to update state with a booklist from the API

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addActionCreator } from '../redux/books/book';
+import { useEffect } from 'react';
+import { addBookAction, fetchAPIBooks } from '../redux/books/book';
 import Form from './Form';
 import DisplayBookList from './DisplayBookList';
 
 const BooksContainer = () => {
   const dispatch = useDispatch();
   const bookDetails = useSelector((state) => state.booksReducer);
+  useEffect(() => {
+    fetchAPIBooks();
+  }, [bookDetails]);
 
   return (
     <div className="container">
       <div className="inner">
-        <Form dispatch={dispatch} action={addActionCreator} />
+        <Form dispatch={dispatch} action={addBookAction} />
         <DisplayBookList
           bookDetails={bookDetails}
         />
